56c7fa3575027203a6aa4e976bdbc2ea6885a747
Matthieu VERGNE <matthieu.vergne@gmail.com>
2011-09-18 20:46:53 +0200

    Complete the Javadoc of Program to have in mind the generic use of the most important classes of this project.

--- dynamicprogram-core/src/main/java/org/dynamicprogram/program/Program.java
+++ dynamicprogram-core/src/main/java/org/dynamicprogram/program/Program.java
@@ -1,7 +1,23 @@
 package org.dynamicprogram.program;
 
+import org.dynamicprogram.program.input.AbstractInputableProgram;
+import org.dynamicprogram.program.input.InputableProgram;
+import org.dynamicprogram.program.output.AbstractOutputableProgram;
+import org.dynamicprogram.program.output.OutputableProgram;
+import org.dynamicprogram.program.update.UpdatableProgram;
+
 /**
- * A program is an instance able to execute a process.
+ * A program is an instance able to execute a process. It is possible to
+ * implement a program extending {@link AbstractProgram}. It is also possible to
+ * improve a program management implementing {@link InputableProgram},
+ * {@link OutputableProgram} and {@link UpdatableProgram}. For these last cases,
+ * you can also extend their abstract classes {@link AbstractInputableProgram},
+ * {@link AbstractOutputableProgram} or abstract {@link UpdatableProgram}, but
+ * only once as you cannot extends several classes in Java. In order to use all
+ * the possibilities of several of these abstract classes (basic implementation,
+ * listeners, etc.), you can just implements the corresponding interfaces and
+ * use an instance of each needed abstract class to implement the corresponding
+ * methods.
  * 
  * @author Matthieu Vergne <matthieu.vergne@gmail.com>
  * 
